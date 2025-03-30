f6d860f515a1bd1e36cd95febffabb77a85f1525
María <mherrezuelo94@hotmail.com>
2025-03-30 14:43:55 +0200

    remove preset vue conf and components, config tailwind, and update favicon and title

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex flex-col items-center justify-center h-screen bg-olive-50">
+    <h1 class="text-6xl font-bold">Welcome to TERRAHQ TRIAL</h1>
+    <p class="text-2xl mt-2">By María Herrezuelo López 👋🏻</p>
+    <p>GitHub Repository: 
+      <a href="https://github.com/mherrezu/terrahq-fe-trial" class="text-secondary-500 hover:text-secondary-400">https://github.com/mherrezu/terrahq-fe-trial?tab=readme-ov-file</a>
+    </p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
 </style>

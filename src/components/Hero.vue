f4f8f870a1c2ac5b2929eed7c0e42f9703ef7755
María <mherrezuelo94@hotmail.com>
2025-03-30 23:39:59 +0200

    header & hero implemented, buttonCta component created, and some styles added

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,18 +1,24 @@
 <template>
-  <section class="container mx-auto  p-10">
-    <p>HERO</p>
-      <p>{{ title }}</p>
-      <p>{{ subtitle }}</p>
-      <p>{{ button_label }}</p>
-      <p>{{ button_link }}</p>
-      <p>{{ bg_image }}</p>
-      <p>{{ shapes.shape_1 }}</p>
-      <p>{{ shapes.shape_2 }}</p>
+  <section
+    class="relative flex flex-col items-center justify-center gap-4 md:gap-8 text-center text-white h-[643px] p-10 bg-cover bg-center"
+    :style="{ backgroundImage: `url(${bg_image})` }">
+
+    <img v-if="shapes.shape_1" :src="shapes.shape_1" class="absolute left-0 bottom-0 h-28 md:h-40 lg:h-82.8 lg:w-110.8" />
+    <img v-if="shapes.shape_2" :src="shapes.shape_2" class="absolute right-0 top-0 h-28 md:h-40 lg:h-82.8 lg:w-110.8" />
+
+    <h1 class="text-5xl md:text-[64px] font-bold">{{ heroTitle }}</h1>
+    <p class="mt-4 max-w-xl">{{ subtitle }}</p>
+    <ButtonCta @click="handleMenu" size="xxl" :color="isFirstAccess ? 'secondary' : 'black'">
+      {{ buttonLabel }}</ButtonCta>
   </section>
 </template>
 
 <script>
+import ButtonCta from "./ButtonCta.vue";
 export default {
+  components: {
+    ButtonCta,
+  },
   props: {
     title: {
       type: Object,
@@ -48,5 +54,30 @@ export default {
       }),
     },
   },
+  data() {
+    return {
+      isFirstAccess: true,
+    };
+  },
+  computed: {
+    heroTitle() {
+      return this.isFirstAccess ? this.title.first_time_accessing : this.title.second_time_accessing;
+    },
+    buttonLabel() {
+      return this.isFirstAccess ? this.button_label.first_time_accessing : this.button_label.second_time_accessing;
+    },
+  },
+  created() {
+    this.checkUserAccess();
+  },
+  methods: {
+    checkUserAccess() {
+      if (localStorage.getItem('isFirstAccess')) {
+        this.isFirstAccess = false;
+      } else {
+        localStorage.setItem('isFirstAccess', 'false');
+      }
+    },
+  }
 };
 </script>

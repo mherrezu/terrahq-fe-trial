37fff1a31b740938c9c999881e1f3776b4027ce5
María <mherrezuelo94@hotmail.com>
2025-03-31 16:52:26 +0200

    Cta component implemented

--- src/components/Cta.vue
+++ src/components/Cta.vue
@@ -1,38 +1,47 @@
 <template>
-    <section class="container mx-auto  p-10">
-    <p>CTA</p>
-      <p>{{ title }}</p>
-      <p>{{ subtitle }}</p>
-      <p>{{ pill }}</p>
-      <p>{{ image_1 }}</p>
-      <p>{{ image_2 }}</p>
+  <section class="px-8 sm:px-14 md:px-24 lg:px-29 pb-15">
+    <div class="flex flex-col sm:flex-row items-center justify-between w-full h-full sm:h-72 lg:h-118.2 bg-olive-50">
+      <div class="sm:w-1/2 h-full text-primary-950 flex flex-col justify-center items-center gap-4 md:gap-6 p-6 lg:px-20">
+        <h2 class="text-2xl lg:text-[40px] font-bold ">{{ title }}</h2>
+        <p class="">{{ subtitle }}</p>
+      </div>
+      <div class="sm:w-1/2 grid grid-cols-2 grid-rows-2 w-full h-full relative">
+        <div class=""></div>
+        <img :src="image_2" alt="Image 2" class="object-cover w-full h-full" />
+        <img :src="image_1" alt="Image 1" class="object-cover w-full h-full" />
+        <Pill :label="pill" pillPosition="bottom-right" />
+    </div>
+    </div>
   </section>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      title: {
-        type: String,
-        default: "",
-      },
-        subtitle: {
-            type: String,
-            default: "",
-        },
-        pill: {
-            type: String,
-            default: "",
-        },
-        image_1: {
-            type: String,
-            default: "",
-        },
-        image_2: {
-            type: String,
-            default: "",
-        },
+</template>
+
+<script>
+import Pill from "./Pill.vue";
+export default {
+  components: {
+    Pill,
+  },
+  props: {
+    title: {
+      type: String,
+      default: "",
     },
-  };
-  </script>
-  
+      subtitle: {
+          type: String,
+          default: "",
+      },
+      pill: {
+          type: String,
+          default: "",
+      },
+      image_1: {
+          type: String,
+          default: "",
+      },
+      image_2: {
+          type: String,
+          default: "",
+      },
+  },
+};
+</script>
---
+++ src/components/Pill.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class=" text-light lg:font-bold text-xs lg:text-sm" :class="{
+    'absolute top-4 right-4 px-3 py-2 rounded-full': pillPosition === 'top-right',
+    'absolute top-4 left-4 px-3 py-2 rounded-full': pillPosition === 'top-left',
+    'absolute bottom-4 left-4 px-3 py-2 rounded-full': pillPosition === 'bottom-left',
+    'absolute bottom-4 right-4 px-3 py-2 rounded-full': pillPosition === 'bottom-right',
+    'bg-secondary-500': pillType === 'Type A',
+    'bg-pink': pillType === 'Type B',
+  }">
+    <span class="">{{ label }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    pillPosition: {
+      type: String,
+      default: "top-right",
+    },
+    pillType: {
+      type: String,
+      default: "Type A",
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
